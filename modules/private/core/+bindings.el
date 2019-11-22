8dece5e4446f8bcf4dd2e6fa0b22c9a867a9cdf9
Björn Larsson <b.e.larsson@gmail.com>
2019-11-22 12:55:12 +0100

    Buffer bindings

--- modules/private/core/+bindings.el
+++ modules/private/core/+bindings.el
@@ -4,4 +4,12 @@
           (:prefix ("t" . "transpose")
             :desc "Chars" :g "c" 'transpose-chars
             :desc "Lines" :g "l" 'transpose-lines
-            :desc "Words" :g "w" 'transpose-words))))
+            :desc "Words" :g "w" 'transpose-words))
+        (:prefix ("b" . "buffer")
+          :desc "New Buffer" :g "N" '+core/new-empty-buffer
+          :desc "Buffer to Clipboard" :g "P" '+core/copy-whole-buffer-to-clipboard
+          :desc "Clipboard to Buffer" :g "Y" '+core/copy-clipboard-to-whole-buffer
+          :desc "Read Only" :g "w" 'read-only-mode
+          :desc "Next Buffer" :g "n" 'next-buffer
+          :desc "Previous Buffer" :g "p" 'previous-buffer
+          :desc "Doom Dashboard" :g "d" '+doom-dashboard/open)))
--- modules/private/core/autoload.el
+++ modules/private/core/autoload.el
@@ -12,3 +12,50 @@
     (string-inflection-java-style-cycle))
    (t
     (string-inflection-ruby-style-cycle))))
+
+;;;###autoload
+(defun +core/indent-region-or-buffer ()
+  "Indent a region if selected, otherwise the whole buffer."
+  (interactive)
+  (unless (member major-mode spacemacs-indent-sensitive-modes)
+    (save-excursion
+      (if (region-active-p)
+          (progn
+            (indent-region (region-beginning) (region-end))
+            (message "Indented selected region."))
+        (progn
+          (evil-indent (point-min) (point-max))
+          (message "Indented buffer.")))
+      (whitespace-cleanup))))
+
+;;;###autoload
+(defun +core/show-and-copy-buffer-filename ()
+  "Show and copy the full path to the current file in the minibuffer."
+  (interactive)
+  ;; list-buffers-directory is the variable set in dired buffers
+  (let ((file-name (or (buffer-file-name) list-buffers-directory)))
+    (if file-name
+        (message (kill-new file-name))
+      (error "Buffer not visiting a file"))))
+
+;;;###autoload
+(defun +core/new-empty-buffer ()
+  "Create a new buffer called untitled(<n>)"
+  (interactive)
+  (let ((newbuf (generate-new-buffer-name "untitled")))
+    (switch-to-buffer newbuf)))
+
+;; http://stackoverflow.com/a/10216338/4869
+;;;###autoload
+(defun +core/copy-whole-buffer-to-clipboard ()
+  "Copy entire buffer to clipboard"
+  (interactive)
+  (clipboard-kill-ring-save (point-min) (point-max)))
+
+;;;###autoload
+(defun +core/copy-clipboard-to-whole-buffer ()
+  "Copy clipboard and replace buffer"
+  (interactive)
+  (delete-region (point-min) (point-max))
+  (clipboard-yank)
+  (deactivate-mark))
