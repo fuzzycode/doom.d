4bd3350bf6fcfbf2b74926649721ff2bdee31657
Björn Larsson <b.e.larsson@gmail.com>
2019-12-13 20:26:13 +0100

    Add A-TAB to toggle h/cpp files

--- +cc.el
+++ +cc.el
@@ -1,5 +1,7 @@
 ;;; ~/.doom.d/+cc.el -*- lexical-binding: t; -*-
 
+(after! (projectile cc-mode)
+  (define-key c++-mode-map (kbd "<A-tab>") #'projectile-find-other-file))
 
 ;;;###package
 (use-package! ff-c-style
