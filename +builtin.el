74ec5ed9ca6cb94aba705e3566992498e7db81aa
Björn Larsson <b.e.larsson@gmail.com>
2019-12-16 19:44:08 +0100

    Setup backup handling

--- +builtin.el
+++ +builtin.el
@@ -21,3 +21,10 @@
 
 (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_GB"))
 (setq ispell-dictionary "english")
+
+;; Backups
+(setq version-control t     ;; Use version numbers for backups.
+      kept-new-versions 10  ;; Number of newest versions to keep.
+      kept-old-versions 0   ;; Number of oldest versions to keep.
+      delete-old-versions t ;; Don't ask to delete excess backup versions.
+      backup-by-copying t)  ;; Copy all files, don't rename them.
