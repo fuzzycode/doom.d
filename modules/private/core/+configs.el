61e591abc611a017d6c19e14cf95fbb16c76a682
Björn Larsson <b.e.larsson@gmail.com>
2020-01-18 21:53:56 +0100

    Add more files to mark a projectile project

--- modules/private/core/+configs.el
+++ modules/private/core/+configs.el
@@ -5,18 +5,11 @@
       undo-tree-visualizer-diff t)
 
 (after! projectile
+
+  (add-to-list 'projectile-project-root-files-bottom-up "compile_commands.json")
+  (add-to-list 'projectile-project-root-files-bottom-up ".lsp-cache")
+  (add-to-list 'projectile-project-root-files-bottom-up ".ccls-cache")
+
   (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map))
 
 (setq ws-butler-convert-leading-tabs-or-spaces t)
-
-(use-package flyspell-correct-ivy
-  :when (featurep! :completion ivy)
-  :bind (:map flyspell-mode-map ("C-;" . #'flyspell-correct-wrapper))
-  :init (setq flyspell-correct-interface #'flyspell-correct-ivy)
-  (map! (:leader
-          (:prefix ("S" . "spelling")
-            (:when (featurep! :tools flyspell)
-              :desc "Correct Next" :g "n" #'flyspell-correct-next
-              :desc "Correct Previous" :g "p" #'flyspell-correct-previous
-              :desc "Correct At Point" :g "c" #'flyspell-correct-at-point
-              :desc "Correct DWIM" :g "s" #'flyspell-correct-wrapper)))))
