57a036da69a39de4ba29be092018097b1691ab52
Björn Larsson <b.e.larsson@gmail.com>
2020-02-08 16:00:32 +0100

    Add shell binding for projectile

--- +projectile.el
+++ +projectile.el
@@ -2,13 +2,14 @@
 (map! (:after projectile
 	(:leader
         (:prefix ("p" . "project")
-          :desc "Shell Command" :g "!" 'projectile-run-shell-command-in-root
-          :desc "Async Shell Command" :g "&" 'projectile-run-async-shell-command-in-root
-          :desc "Edit dir-locals" :g "e" 'projectile-edit-dir-locals
-          :desc "Compile" :g "c" 'projectile-compile-project
-          :desc "Configure" :g "C" 'projectile-configure-project
-          :desc "Dired" :g "d" 'projectile-dired
-          :desc "Kill Buffers" :g "k" 'projectile-kill-buffers
-          :desc "Test Project" :g "t" 'projectile-test-project
+          :desc "Shell Command" :g "!" #'projectile-run-shell-command-in-root
+          :desc "Async Shell Command" :g "&" #'projectile-run-async-shell-command-in-root
+          :desc "Edit dir-locals" :g "e" #'projectile-edit-dir-locals
+          :desc "Compile" :g "c" #'projectile-compile-project
+          :desc "Configure" :g "C" #'projectile-configure-project
+          :desc "Dired" :g "d" #'projectile-dired
+          :desc "Kill Buffers" :g "k" #'projectile-kill-buffers
+          :desc "Test Project" :g "t" #'projectile-test-project
+          :desc "Shell" :g "s" #'projectile-run-vterm
           :desc "Find file in other project" :g "O" #'doom/find-file-in-other-project
           :desc "Project Scratch Buffer" :g "x" #'doom/open-project-scratch-buffer))))
