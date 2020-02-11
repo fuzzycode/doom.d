4661b28f3c1521e05a759e7ad33079416adece46
Björn Larsson <b.e.larsson@gmail.com>
2020-02-11 21:21:49 +0100

    Add more projectile key bindings

--- +projectile.el
+++ +projectile.el
@@ -19,3 +19,6 @@
         (:leader
           (:prefix ("p" . "project")
             :desc "Switch Perspective" :g "P" #'persp-switch))))
+
+(after! projectile
+  (global-set-key (kbd "M-o") #'projectile-find-file-dwim))
