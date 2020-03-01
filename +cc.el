b0aa73aa7678042e43a054805a06b1409edcafea
Björn Larsson <b.e.larsson@gmail.com>
2020-03-01 13:35:41 +0100

    Make .h files default to c++ mode

--- +cc.el
+++ +cc.el
@@ -1,5 +1,8 @@
 ;;; ~/.doom.d/+cc.el -*- lexical-binding: t; -*-
 
+;; Make .h file default to cpp-mode
+(add-to-list 'auto-mode-alist '("\\.h$" . c++-mode))
+
 (after! (projectile cc-mode)
   (define-key c++-mode-map (kbd "<A-tab>") #'projectile-find-other-file))
 
