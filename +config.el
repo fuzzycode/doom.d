17c2cfa2e2123d694637a23982acc786214612c0
Björn Larsson <b.e.larsson@gmail.com>
2020-04-05 15:27:38 +0200

    Add subword mode to prog-mode

--- +config.el
+++ +config.el
@@ -1,5 +1,10 @@
 ;;; ~/Development/GitHub/dotfiles/doom.d/+config.el -*- lexical-binding: t; -*-
 
+;;;###package
 (use-package! authinfo-mode
   :defer t
   :mode "authinfo\\(\\.gpg\\)?$")
+
+;;;###package
+(use-package! subword
+  :hook (prog-mode . subword-mode))
