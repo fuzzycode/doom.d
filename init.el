aa78bb80a552a7c89f513b838e9c042c257fe0e7
Björn Larsson <b.e.larsson@gmail.com>
2020-05-05 16:13:34 +0200

    Enable lsp for latex

--- init.el
+++ init.el
@@ -96,7 +96,7 @@
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       (latex +latexmk)            ; writing papers in Emacs has never been so fun
+       (latex +latexmk +lsp)            ; writing papers in Emacs has never been so fun
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
