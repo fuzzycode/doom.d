e7937c1a7bffcb68e0a687927aaf8869053b1a44
Björn Larsson <b.e.larsson@gmail.com>
2020-07-01 22:24:50 +0200

    Remove no longer needed modeline hack

--- +ui.el
+++ +ui.el
@@ -11,8 +11,4 @@
         doom-modeline-buffer-file-name-style 'truncate-with-project
         doom-modeline-env-version t
         doom-modeline-mu4e t
-        doom-modeline-vcs-max-length 45)
-
-  (doom-modeline-def-modeline 'main
-    '(bar workspace-name window-number modals matches buffer-info remote-host buffer-position word-count parrot selection-info)
-    '(objed-state misc-info persp-name battery grip irc mu4e gnus github debug lsp minor-modes input-method indent-info buffer-encoding major-mode process vcs checker " " bar)))
+        doom-modeline-vcs-max-length 45))
