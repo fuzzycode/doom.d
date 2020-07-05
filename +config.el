836ff6befe3c18512484e4c9663d346ae17b3591
Björn Larsson <b.e.larsson@gmail.com>
2020-07-05 18:50:57 +0200

    Remove authinfo mode
    No longer relevant on Emacs 27 that comes with a authinfo mode bundled.

--- +config.el
+++ +config.el
@@ -1,10 +1,5 @@
 ;;; ~/Development/GitHub/dotfiles/doom.d/+config.el -*- lexical-binding: t; -*-
 
 ;;;###package
-(use-package! authinfo-mode
-  :defer t
-  :mode "authinfo\\(\\.gpg\\)?$")
-
-;;;###package
 (use-package! subword
   :hook (prog-mode . subword-mode))
--- packages.el
+++ packages.el
@@ -40,8 +40,5 @@
 ;; CPP MODE
 (package! ff-c-style :recipe (:host github :repo "fuzzycode/ff-c-style"))
 
-;; Authinfo files
-(package! authinfo-mode :recipe (:host github :repo "fuzzycode/authinfo-mode"))
-
 ;; SHELL SCRIPT
 ;(package! sh-script :built-in)
