c20c19355264999cabd01e9f149ce7779c5b5e82
Björn Larsson <b.e.larsson@gmail.com>
2020-08-31 21:25:21 +0200

    Make YASnippet Table bigger

--- modules/private/core/+popups.el
+++ modules/private/core/+popups.el
@@ -1,3 +1,5 @@
 ;;; private/core/+popups.el -*- lexical-binding: t; -*-
 
 (set-popup-rule! "^\\*Shell Command Output\\*$" :quit 'other :side 'bottom :height 40 :select nil :actions '(+bl/special-mode-action-fn))
+
+(set-popup-rule! "^\\*YASnippet Tables\\*$" :quit 'other :side 'bottom :height 0.5 :select t)
