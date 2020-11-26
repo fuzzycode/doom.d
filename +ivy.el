083a126232aee623763c593a5b361331ea941f75
Björn Larsson <b.e.larsson@gmail.com>
2020-11-26 21:12:22 +0100

    Add initial input for magit-checkout

--- +ivy.el
+++ +ivy.el
@@ -38,6 +38,8 @@
         ivy-fixed-height-minibuffer t
         ivy-display-style 'fancy)
 
+  (add-to-list 'ivy-initial-inputs-alist '(magit-checkout . "^"))
+
   (global-set-key (kbd "M-m") #'counsel-imenu))
 
 (after! ivy-prescient
