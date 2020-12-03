8ceffb07486e2b1613c910b5f67da63d77bbc914
Björn Larsson <b.e.larsson@gmail.com>
2020-12-03 21:25:03 +0100

    Move core doctor to main config

--- doctor.el
+++ doctor.el
@@ -2,3 +2,13 @@
 
 (unless (executable-find "git-imerge")
   (warn! "Unable to find git imerge executable, magit imerge will not work as expected."))
+
+(unless (executable-find "pandoc")
+  (warn! "pandoc executable not found. Make sure it is installed."))
+
+(unless (executable-find "terminal-notifier")
+  (warn! "terminal-notifier was not found. Make sure its installed to get nice notifications."))
+
+(when (featurep! :completion ivy)
+  (unless (executable-find "fzf")
+    (warn! "fzf not found on your system, counsel-fzf will not work.")))
