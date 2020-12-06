0317f810b8a00bb78fce91e6987a862f0cbd5589
Björn Larsson <b.e.larsson@gmail.com>
2020-12-06 18:17:44 +0100

    Prepare debugger module for modal editing

--- modules/tools/debugger/config.el
+++ modules/tools/debugger/config.el
@@ -20,8 +20,7 @@
                   (lambda (session)
                     (when (eq session session-at-creation)
                       (+dap-running-session-mode -1)))))))
-  :hook ((after-init . dap-mode)
-         (dap-mode . dap-ui-mode)
+  :hook ((dap-mode . dap-ui-mode)
          (c-mode-local-vars . +debugging/init-dap-mode-cpp)
          (c++-mode-local-vars . +debugging/init-dap-mode-cpp)
          (dap-session-created . #'+dap-running-session-mode)
@@ -47,42 +46,42 @@
   (map! (:localleader
          :mode (c++-mode c-mode python-mode)
          (:prefix ("d" . "debug")
-          :desc "DAP REPL" :g "'" #'dap-ui-repl
-          :desc "DAP Hydra" :g "." #'dap-hydra
-          :desc "Delete All Sessions" :g "A" #'dap-delete-all-sessions
-          :desc "Continue" :g "c" #'dap-continue
-          :desc "Step Into" :g "i" #'dap-step-in
-          :desc "Step Out" :g "o" #'dap-step-out
-          :desc "Restart Frame" :g "r" #'dap-restart-frame
-          :desc "Step (Next)" :g "s" #'dap-next
-          :desc "Inspect Thing At Point" :g "v" #'dap-ui-inspect-thing-at-point
+          :desc "DAP REPL" :ng "'" #'dap-ui-repl
+          :desc "DAP Hydra" :ng "." #'dap-hydra
+          :desc "Delete All Sessions" :ng "A" #'dap-delete-all-sessions
+          :desc "Continue" :ng "c" #'dap-continue
+          :desc "Step Into" :ng "i" #'dap-step-in
+          :desc "Step Out" :ng "o" #'dap-step-out
+          :desc "Restart Frame" :ng "r" #'dap-restart-frame
+          :desc "Step (Next)" :ng "s" #'dap-next
+          :desc "Inspect Thing At Point" :ng "v" #'dap-ui-inspect-thing-at-point
           (:prefix ("b" . "breakpoints")
-           :desc "Add Breakpoint" :g "a" #'dap-breakpoint-add
-           :desc "Toggle Breakpoint" :g "b" #'dap-breakpoint-toggle
-           :desc "Breakpoint Condition" :g "c" #'dap-breakpoint-condition
-           :desc "Delete All" :g "D" #'dap-breakpoint-delete-all
-           :desc "Delete Breakpoint" :g "d" #'dap-breakpoint-delete
-           :desc "Hit Condition" :g "h" #'dap-breakpoint-hit-condition
-           :desc "Log Message" :g "l" #'dap-breakpoint-log-message)
+           :desc "Add Breakpoint" :ng "a" #'dap-breakpoint-add
+           :desc "Toggle Breakpoint" :ng "b" #'dap-breakpoint-toggle
+           :desc "Breakpoint Condition" :ng "c" #'dap-breakpoint-condition
+           :desc "Delete All" :ng "D" #'dap-breakpoint-delete-all
+           :desc "Delete Breakpoint" :ng "d" #'dap-breakpoint-delete
+           :desc "Hit Condition" :ng "h" #'dap-breakpoint-hit-condition
+           :desc "Log Message" :ng "l" #'dap-breakpoint-log-message)
           (:prefix ("d" . "debugging")
-           :desc "Debug" :g "d" #'dap-debug
-           :desc "Edit Template" :g "e" #'dap-debug-edit-template
-           :desc "Debug Last" :g "l" #'dap-debug-last
-           :desc "Debug Recent" :g "r" #'dap-debug-recent)
+           :desc "Debug" :ng "d" #'dap-debug
+           :desc "Edit Template" :ng "e" #'dap-debug-edit-template
+           :desc "Debug Last" :ng "l" #'dap-debug-last
+           :desc "Debug Recent" :ng "r" #'dap-debug-recent)
           (:prefix ("e" . "Eval")
-           :desc "Eval" :g "e" #'dap-eval
-           :desc "Eval region" :g "r" #'dap-eval-region
-           :desc "Eval Thing at Point" :g "t" #'dap-eval-thing-at-point)
+           :desc "Eval" :ng "e" #'dap-eval
+           :desc "Eval region" :ng "r" #'dap-eval-region
+           :desc "Eval Thing at Point" :ng "t" #'dap-eval-thing-at-point)
           (:prefix ("I" . "inspect")
-           :desc "Inspect" :g "i" #'dap-ui-inspect
-           :desc "Inspect Region" :g "r" #'dap-ui-inspect-region
-           :desc "Inspect Thing at Point" :g "t" #'dap-ui-inspect-thing-at-point)
+           :desc "Inspect" :ng "i" #'dap-ui-inspect
+           :desc "Inspect Region" :ng "r" #'dap-ui-inspect-region
+           :desc "Inspect Thing at Point" :ng "t" #'dap-ui-inspect-thing-at-point)
           (:prefix ("S" . "switch")
-           :desc "Switch Stack Frame" :g "f" #'dap-switch-stack-frame
-           :desc "Switch Session" :g "s" #'dap-switch-session
-           :desc "Switch Thread" :g "t" #'dap-switch-thread)
+           :desc "Switch Stack Frame" :ng "f" #'dap-switch-stack-frame
+           :desc "Switch Session" :ng "s" #'dap-switch-session
+           :desc "Switch Thread" :ng "t" #'dap-switch-thread)
           (:prefix ("w" . "debug windows")
-           :desc "Breakpoints" :g "b" #'dap-ui-breakpoints
-           :desc "Locals" :g "l" #'dap-ui-locals
-           :desc "Go to Output Buffer" :g "o" #'dap-go-to-output-buffer
-           :desc "Sessions" :g "s" #'dap-ui-sessions)))))
+           :desc "Breakpoints" :ng "b" #'dap-ui-breakpoints
+           :desc "Locals" :ng "l" #'dap-ui-locals
+           :desc "Go to Output Buffer" :ng "o" #'dap-go-to-output-buffer
+           :desc "Sessions" :ng "s" #'dap-ui-sessions)))))
