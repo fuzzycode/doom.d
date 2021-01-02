974a4af3c9c4ad5926fb653c9f3865ec41a68fe3
Björn Larsson <b.e.larsson@gmail.com>
2021-01-02 12:41:42 +0100

    Fix normal mode binding

--- +packages.el
+++ +packages.el
@@ -403,22 +403,22 @@
   :defer t
   :mode "\\.csv$"
   :init (map! :localleader :map csv-mode-map
-              :desc "Align Field" :g "a" #'csv-align-field
-              :desc "Kill Fields" :g "k" #'csv-kill-fields
-              :desc "Header Line" :g "h" #'csv-header-line
-              :desc "Toggle Invisibility" :g "" #'csv-toggle-invisibility
-              :desc "Forward Field" :g "i" #'csv-forward-field
-              :desc "Backward Field" :g "n" #'csv-backward-field
-              :desc "Reverse region" :g "p" #'csv-reverse-region
+              :desc "Align Field" :ng "a" #'csv-align-field
+              :desc "Kill Fields" :ng "k" #'csv-kill-fields
+              :desc "Header Line" :ng "h" #'csv-header-line
+              :desc "Toggle Invisibility" :ng "" #'csv-toggle-invisibility
+              :desc "Forward Field" :ng "i" #'csv-forward-field
+              :desc "Backward Field" :ng "n" #'csv-backward-field
+              :desc "Reverse region" :ng "p" #'csv-reverse-region
               (:prefix ("s" . "sort")
-                :desc "Sort Fields" :g "f" #'csv-sort-fields
-                :desc "Sort Numeric Fields" :g "n" #'csv-sort-numeric-fields
-                :desc "Toggle Descending" :g "o" #'csv-toggle-descending)
-              :desc "Transpose" :g "t" #'csv-transpose
-              :desc "Unalign Fields" :g "u" #'csv-unalign-fields
+                :desc "Sort Fields" :ng "f" #'csv-sort-fields
+                :desc "Sort Numeric Fields" :ng "n" #'csv-sort-numeric-fields
+                :desc "Toggle Descending" :ng "o" #'csv-toggle-descending)
+              :desc "Transpose" :ng "t" #'csv-transpose
+              :desc "Unalign Fields" :ng "u" #'csv-unalign-fields
               (:prefix ("y" . "yank")
-                :desc "Yank Fields" :g "f" #'csv-yank-fields
-                :desc "Yank As New Table" :g "t" #'csv-yank-as-new-table)))
+                :desc "Yank Fields" :ng "f" #'csv-yank-fields
+                :desc "Yank As New Table" :ng "t" #'csv-yank-as-new-table)))
 
 ;;;###package
 (use-package! ebib
@@ -426,4 +426,4 @@
   :commands (ebib)
   :init (map! (:leader
                (:prefix "a"
-                :desc "Ebib" :g "e" #'ebib))))
+                :desc "Ebib" :ng "e" #'ebib))))
