61d77ef08fbde2a616cef1fada646c8141004719
Björn Larsson <b.e.larsson@gmail.com>
2021-05-18 21:13:01 +0200

    Remove dedicated popups config

--- +core.el
+++ +core.el
@@ -194,3 +194,10 @@
         mac-option-modifier 'alt
         mac-right-command-modifier nil
         mac-right-option-modifier nil))
+;;
+;; POPUPS
+;;
+
+(set-popup-rule! "^\\*Shell Command Output\\*$" :quit 'other :side 'bottom :height 40 :select nil :actions '(+bl/special-mode-action-fn))
+
+(set-popup-rule! "^\\*YASnippet Tables\\*$" :quit 'other :side 'bottom :height 0.5 :select t)
