f84ea7658ca7ade449f24a1360daa34e620f89dc
Björn Larsson <b.e.larsson@gmail.com>
2021-08-04 21:04:39 +0200

    Remove dead code

--- config.el
+++ config.el
@@ -8,36 +8,10 @@
       doom-big-font (font-spec :family "JetBrains Mono" :size 24)
       line-spacing 1)
 
-;; Setup all standard prefixes
-;; (map! (:leader
-;;        ;; (:prefix ("a" . "applications"))
-;;        (:prefix ("b" . "buffer/bookmarks"))
-;;        (:prefix ("c" . "code"))
-;;        (:prefix ("d" . "documentation"))
-;;        (:prefix ("e" . "errors"))
-;;        (:prefix ("f" . "files"))
-;;        (:prefix ("g" . "git"))
-;;        (:prefix ("h" . "help"))
-;;        (:prefix ("i" . "insert"))
-;;        (:prefix ("j" . "jump/join"))
-;;        (:prefix ("k" . "pairs"))
-;;        (:prefix ("l" . "lines"))
-;;        (:prefix ("m" . "mode leader"))
-;;        (:prefix ("o" . "org"))
-;;        (:prefix ("p" . "project"))
-;;        (:prefix ("q" . "quit/reload"))
-;;        (:prefix ("r" . "resume"))
-;;        (:prefix ("s" . "search"))
-;;        (:prefix ("S" . "spelling"))
-;;        (:prefix ("t" . "toggle"))
-;;        (:prefix ("w" . "windows"))
-;;        (:prefix ("x" . "text"))))
-
 ;Load Specific files
 (load! "+bindings")
 (load! "+core")
-;; (load! "+ivy")
-;; (load! "+org")
+(load! "+org")
 (load! "+packages")
 
 ;; Allow for machine local customizations
