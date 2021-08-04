b4b8fd0b8a578df30b03a499df4eba466df82ead
Björn Larsson <b.e.larsson@gmail.com>
2021-08-04 21:04:15 +0200

    Add help key bindings

--- +bindings.el
+++ +bindings.el
@@ -18,7 +18,53 @@
       (:prefix "t"
        :desc "Trailing Whitespace" :ng "w" (cmd! (setq show-trailing-whitespace (not show-trailing-whitespace)))
        (:after lsp-mode
-         :desc "Breadcrumb Mode" :ng "h" #'lsp-headerline-breadcrumb-mode)))
+         :desc "Breadcrumb Mode" :ng "h" #'lsp-headerline-breadcrumb-mode))
+
+      ;; Help
+      (:prefix "h"
+       :desc "Info" "i" #'info
+       :desc "Emacs News" "n" #'view-emacs-news
+       :desc "Emacs Tutorial" "t" #'help-with-tutorial
+
+       (:prefix ("a" . "apropos")
+        :desc "Apropos" "a" #'apropos
+        :desc "Command" "c" #'apropos-command
+        :desc "Documentation" "d" #'apropos-documentation
+        :desc "Documentation Property" "D" #'apropos-documentation-property
+        :desc "Internal" "i" #'apropos-internal
+        :desc "Library" "l" #'apropos-library
+        :desc "Local Value" "V" #'apropos-local-value
+        :desc "Local Variable" "E" #'apropos-local-variable
+        :desc "Read Pattern" "r" #'apropos-read-pattern
+        :desc "User Option" "o" #'apropos-user-option
+        :desc "Value" "v" #'apropos-value
+        :desc "Variable" "e" #'apropos-variable)
+       (:prefix ("d" . "describe")
+        :desc "Autodefs" "a" #'doom/describe-autodefs
+        :desc "Char" "c" #'describe-char
+        :desc "Bindings" "b" #'describe-bindings
+        :desc "Doom Module" "d" #'doom/describe-module
+        :desc "Function" "f" #'describe-function
+        :desc "Face" "F" #'describe-face
+        :desc "Key" "k" #'describe-key
+        :desc "Language Environment" "L" #'describe-language-environment
+        :desc "Mode" "m" #'describe-mode
+        :desc "Active Mode" "M" #'doom/describe-active-minor-mode
+        :desc "Symbol" "s" #'describe-symbol
+        :desc "Package" "p" #'describe-package
+        :desc "Doom Package" "P" #'doom/describe-package
+        :desc "Theme" "t" #'describe-theme
+        :desc "Variable" "v" #'describe-variable
+        :desc "Text Properties" "T" #'describe-text-properties)
+       (:prefix ("D" . "Doom")
+        :desc "Info" "i" #'doom/info
+        :desc "Issue Tracker" "I" #'doom/issue-tracker
+        :desc "Homepage" "h" #'doom/homepage
+        :desc "News" "n" #'doom/help-news
+        :desc "Discourse" "d" #'doom/discourse
+        :desc "Doom Manual" "D" #'doom/help
+        :desc "Report Bug" "r" #'doom/report-bug
+        :desc "Version" "V" #'doom/version)))
 
 ;; Bindings with no leader key
 (map!
