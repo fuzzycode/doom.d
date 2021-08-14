1d741272a88f23e3597da91ad961c9f438f0b176
Björn Larsson <b.e.larsson@gmail.com>
2021-08-14 19:09:51 +0200

    Add more font configurations

--- config.el
+++ config.el
@@ -6,6 +6,9 @@
 
 (setq doom-font (font-spec :family "JetBrains Mono" :size 15)
       doom-big-font (font-spec :family "JetBrains Mono" :size 24)
+      doom-variable-pitch-font (font-spec :family "Overpass" :size 15)
+      doom-unicode-font (font-spec :family "JuliaMono")
+      doom-serif-font (font-spec :family "IBM Plex Mono" :weight 'light)
       line-spacing 1)
 
 ;Load Specific files
