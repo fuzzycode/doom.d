b5c9769fbea86d484e34a47c62ea7c830b4d7ce4
Björn Larsson <b.e.larsson@gmail.com>
2021-08-26 21:45:03 +0200

    Bump packages

--- packages.el
+++ packages.el
@@ -1,13 +1,13 @@
 ;; -*- lexical-binding: t; -*-
 
 ;; C++
-(package! ninja-mode :pin "ffb47fcf82ed12f53c046f6d51401c34ec35eced")
+(package! ninja-mode :pin "e90dfd3c7528b9c620eab29121a3591af7bf035e")
 (package! ff-c-style :recipe (:host github :repo "fuzzycode/ff-c-style") :pin "32bc21d4f0e011dbdb7dc1a9d1cd8651353f2943")
 (package! sourcetrail :pin "b8d5557aa565ae979622312576db20515f65f977")
 
 ;; GIT
 (package! magit-imerge :pin "cf3b4646aa0205e8d7f47e45165fe6403d6440f5")
-(package! git-commit :pin "4e1041b1d9c0aa322d92fe5ba5fea06262a81a8b")
+(package! git-commit :pin "5a0cf9aaa9acf53c68c0fe98883c081aa5e29dd3")
 (package! gitignore-mode :pin "433e1c57a63c88855fc41a942e29d7bc8c9c16c7")
 (package! gitconfig-mode :pin "433e1c57a63c88855fc41a942e29d7bc8c9c16c7")
 (package! gitattributes-mode :pin "433e1c57a63c88855fc41a942e29d7bc8c9c16c7")
@@ -17,10 +17,10 @@
 ;;ORG
 (package! demo-it :pin "9cfa5c3f92a0dca7eebb1f1a2011643c9b009d26")
 (package! org-super-agenda :pin "a5557ea4f51571ee9def3cd9a1ab1c38f1a27af7")
-(package! doct :pin "67fc46c8a68989b932bce879fbaa62c6a2456a1f")
+(package! doct :pin "c7c8687ae8a7f1230732eaebc89ea668b4f7a37d")
 (package! org-make-toc :pin "26fbd6a7e1e7f8e473fe3a5f74faec715c3a05aa")
 (package! org-ql :pin "94f9e6f3031b32cf5e2149beca7074807235dcb0")
-(package! org-appear :pin "148aa124901ae598f69320e3dcada6325cdc2cf0")
+(package! org-appear :pin "a1aa8496f2fd61305e43e03e6eeee2ff92aa9e24")
 (package! ox-gfm :pin "99f93011b069e02b37c9660b8fcb45dab086a07f")
 (package! ox-asciidoc :pin "efb74df1179702e19ce531f84993ac5b5039075f")
 
@@ -43,7 +43,7 @@
 (package! smart-backspace :pin "a10ec44ff325ec8c4c98b1a6e44e89e60a9aa4ac")
 (package! open-junk-file :pin "558bec7372b0fed4c4cb6074ab906535fae615bd")
 (package! winum :pin "c5455e866e8a5f7eab6a7263e2057aff5f1118b9")
-(package! pandoc-mode :pin "2ad3cfa3045e0a3bcbdee02819c6fd356a8bc40c")
+(package! pandoc-mode :pin "39167ff0e9293b4632cf162a32c9d0b6990a371d")
 (package! avy :pin "e92cb37457b43336b765630dbfbea8ba4be601fa")
 (package! centered-cursor-mode :pin "4093821cc9759ca5a3c6e527d4cc915fc3a5ad74")
 (package! smart-newline :pin "0553a9e4be7188352de1a28f2eddfd28e7436f94")
