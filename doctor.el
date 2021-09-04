480e7252b24e1064dcd59576da23133510ed93c1
Björn Larsson <b.e.larsson@gmail.com>
2021-09-04 08:27:59 +0200

    Add font check to local doctor file

--- doctor.el
+++ doctor.el
@@ -12,3 +12,17 @@
 (when (featurep! :completion ivy)
   (unless (executable-find "fzf")
     (warn! "fzf not found on your system, counsel-fzf will not work.")))
+
+(print! (start "Checking for installed fonts ... "))
+(with-temp-buffer
+  (cl-destructuring-bind (status . output)
+      (doom-call-process "fc-list" "--format=%{family[0]}\n")
+    (if (not (zerop status))
+        (print! (error "Issue running `fc-list' on your system"))
+      (insert output)
+      (dolist (font (remove-duplicates (list doom-font doom-unicode-font doom-big-font doom-variable-pitch-font doom-serif-font)
+                                       :test (lambda (lhs rhs) (eq (font-get lhs :family) (font-get rhs :family)))))
+        (let ((family (format "%s" (font-get font :family))))
+          (if (save-excursion (re-search-backward family nil t))
+              (print! (success "Found font family: %s") family)
+            (print! (warn "Unable to find font family: %s") family)))))))
