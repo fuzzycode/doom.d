0df5eaf86f3386b362c3c86d4cc34092f61d51f5
Björn Larsson <b.e.larsson@gmail.com>
2021-10-26 09:47:50 +0200

    Bump private packages

--- packages.el
+++ packages.el
@@ -1,29 +1,26 @@
 ;; -*- lexical-binding: t; -*-
 
 ;; C++
-(package! ninja-mode :pin "a280868e9c2c791a0d1529c7002786a117bd16fc")
+(package! ninja-mode :recipe (:files ("*.el")) :pin "0cd88287a4cd77d11c92c7a9b44bb15fb787a1ee")
 (package! ff-c-style :recipe (:host github :repo "fuzzycode/ff-c-style") :pin "32bc21d4f0e011dbdb7dc1a9d1cd8651353f2943")
 
 ;; GIT
 (when (featurep! :tools magit)
-  (package! magit-imerge :pin "bd548da8b0c982c346c5c3d11641b9602415ab74")
-  (package! git-commit :pin "2049fd6f6eae7e958b673e809299bc7d3f02a781")
-  (package! gitignore-mode :pin "433e1c57a63c88855fc41a942e29d7bc8c9c16c7")
-  (package! gitconfig-mode :pin "433e1c57a63c88855fc41a942e29d7bc8c9c16c7")
-  (package! gitattributes-mode :pin "433e1c57a63c88855fc41a942e29d7bc8c9c16c7")
+  (package! magit-imerge :pin "1ee213d7fa1536c86c128d09946b44ededbfac9c")
+  (package! git-commit :pin "aba0a596115b42fbd60347d893bcc319020ce5a2")
   (package! rigid-tabs :pin "eba84ceaba2e57e76ad2dfbb7a7154238a25d956")
   (package! gitignore-templates :pin "d28cd1cec00242b688861648d36d086818b06099"))
 
 ;;ORG
 (when (featurep! :lang org)
   (package! demo-it :pin "9cfa5c3f92a0dca7eebb1f1a2011643c9b009d26")
-  (package! org-super-agenda :pin "a5557ea4f51571ee9def3cd9a1ab1c38f1a27af7")
-  (package! doct :pin "bf8ba74cf29c876958e8c7249e044d7800145f9d")
+  (package! org-super-agenda :pin "fb5e2ef277bc811a3b061106c99e4c47b6b86f80")
+  (package! doct :pin "c1919a4297e5479d3a22ded90095245317b29935")
   (package! org-make-toc :pin "26fbd6a7e1e7f8e473fe3a5f74faec715c3a05aa")
   (package! org-ql :pin "31aeb0a2505acf8044c07824888ddec7f3e529c1")
   (package! org-appear :pin "a1aa8496f2fd61305e43e03e6eeee2ff92aa9e24")
   (package! ox-gfm :pin "99f93011b069e02b37c9660b8fcb45dab086a07f")
-  (package! ox-asciidoc :pin "c2b794aae26133189499424569d0f88b41c7e4c6"))
+  (package! ox-asciidoc :pin "d60ac439278cec214882f92c47bc16e0f43ae98e"))
 
 ;;ELISP
 (package! eval-sexp-fu :pin "36d2fe3bcf602e15ca10a7f487da103515ef391a")
@@ -45,7 +42,7 @@
 (package! open-junk-file :pin "558bec7372b0fed4c4cb6074ab906535fae615bd")
 (package! pandoc-mode :pin "bf01a14e99304653ae722226ea064c7d4b641774")
 (package! centered-cursor-mode :pin "4093821cc9759ca5a3c6e527d4cc915fc3a5ad74")
-(package! ssh-config-mode :pin "2642659aa4cb882d95d84f780e8f8bf5e3a9114b")
+(package! ssh-config-mode :pin "d560a0876a93ad4130baf33dae1b9405ad37a405")
 (package! smart-newline :pin "0553a9e4be7188352de1a28f2eddfd28e7436f94")
 (package! hardhat :pin "908cb130be3d56921a3687a00b974ba5eef3a11f")
 (package! fix-word :pin "e967dd4ac98d777deeede8b497d6337634c06df4")
