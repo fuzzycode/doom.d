0df31ecdbadcb53105f1574782d95b2c57d3747f
Björn Larsson <b.e.larsson@gmail.com>
2022-01-20 16:27:02 +0100

    Improve config

--- init.el
+++ init.el
@@ -60,7 +60,7 @@
        gist
        (lookup +dictionary +docsets)
        (lsp +peek)
-       (magit (:if (not IS-WINDOWS) +forge ))
+       (magit (:unless IS-WINDOWS) +forge ))
        pdf
        prodigy
        rgb
