aa3cce520f46023fdf4897f89cb24ed26990e007
Björn Larsson <b.e.larsson@gmail.com>
2022-10-08 19:10:47 +0200

    Bump private packages

--- packages.el
+++ packages.el
@@ -2,34 +2,34 @@
 
 
 ;; C++
-(package! ninja-mode :recipe (:files ("*.el")) :pin "55f54511d35716c43637dee2bcb5fbc7839f967b")
+(package! ninja-mode :recipe (:files ("*.el")) :pin "3a6a4ed84bf4a310e5aa032d77844bfa6744c4cb")
 (package! ff-c-style :recipe (:host github :repo "fuzzycode/ff-c-style") :pin "32bc21d4f0e011dbdb7dc1a9d1cd8651353f2943")
 
-(package! shader-mode :pin "d7dc8d0d6fe8914e8b6d5cf2081ad61e6952359c") ;; This is also a part of the csharp module but I only want this package from it
+(package! shader-mode :pin "fe5a1982ba69e4a98b834141a46a1908f132df15") ;; This is also a part of the csharp module but I only want this package from it
 
 ;; GIT
 (when (modulep! :tools magit)
   (when IS-WINDOWS
     (package! ssh-agency :pin "a5377e4317365a3d5442e06d5c255d4a7c7618db"))
-  (package! magit-imerge :pin "37bca48218dc32cad964e01e0f9936a90f634fba")
-  (package! git-commit :pin "a7953b2645503904b2a31e18e019f07af9e71a7a")
+  (package! magit-imerge :pin "5a1833d33e1516c7819521a5b35135527d9409f9")
+  (package! git-commit :pin "717171265026c0913a3611251f9d1be727a8890b")
   (package! rigid-tabs :pin "872a10c8751574c9610cba1800f541a6eda24997")
   (package! gitignore-templates :pin "d28cd1cec00242b688861648d36d086818b06099"))
 
 ;;ORG
 (when (modulep! :lang org)
   (package! demo-it :pin "e399fd7ceb73caeae7cb50b247359bafcaee2a3f")
-  (package! org-super-agenda :pin "3108bc3f725818f0e868520d2c243abe9acbef4e")
-  (package! doct :pin "8464809754f3316d5a2fdcf3c01ce1e8736b323b")
+  (package! org-super-agenda :pin "f4f528985397c833c870967884b013cf91a1da4a")
+  (package! doct :pin "506c22f365b75f5423810c4933856802554df464")
   (package! org-make-toc :pin "26fbd6a7e1e7f8e473fe3a5f74faec715c3a05aa")
-  (package! org-ql :pin "67506a56f8e84af80af922e40797f8dc861243e4")
-  (package! org-appear :pin "8dd1e564153d8007ebc4bb4e14250bde84e26a34")
-  (package! ox-gfm :pin "99f93011b069e02b37c9660b8fcb45dab086a07f")
-  (package! ox-asciidoc :pin "c8bc184f9088b76fdf1ce20e6e5d0a1588e1b327"))
+  (package! org-ql :pin "5f70636556bffca92d8ef8297ba3002a4ab5b52d")
+  (package! org-appear :pin "60ba267c5da336e75e603f8c7ab3f44e6f4e4dac")
+  (package! ox-gfm :pin "46faa67dbb3fb0cd7a76c3fe518f16e4195c22c7")
+  (package! ox-asciidoc :pin "a55ac6adef39124c9434be47fe9cc0c75c4bfea2"))
 
 ;;ELISP
 (when (modulep! :lang emacs-lisp)
-  (package! emacs-inspector :recipe (:host github :repo "mmontone/emacs-inspector") :pin "8c555dfadb737bf2f05ff2786dbab2ac985775ef")
+  (package! emacs-inspector :recipe (:host github :repo "mmontone/emacs-inspector") :pin "0e89d28558f57db4519f154bb72ce617a8c6265d")
   (package! eval-sexp-fu :pin "36d2fe3bcf602e15ca10a7f487da103515ef391a"))
 
 ;; MAIL
@@ -43,21 +43,21 @@
 (package! swedish-holidays :recipe (:host github :repo "fuzzycode/swedish-holidays"))
 (package! visual-regexp-steroids :pin "a6420b25ec0fbba43bf57875827092e1196d8a9e")
 (package! visual-regexp :pin "48457d42a5e0fe10fa3a9c15854f1f127ade09b5")
-(package! expand-region :pin "7e5bbe2763c12bae3e77fe0c49bcad05ff91dbfe")
+(package! expand-region :pin "c5c4362741deebb0985a8a29f9b8b0e25160764a")
 (package! sort-words :pin "7b6e108f80237363faf7ec28b2c58dec270b8601")
 (package! smart-backspace :pin "a10ec44ff325ec8c4c98b1a6e44e89e60a9aa4ac")
 (package! open-junk-file :pin "558bec7372b0fed4c4cb6074ab906535fae615bd")
-(package! pandoc-mode :pin "2a4e726a29d38e7c2379787cad619e5392ad2da0")
-(package! centered-cursor-mode :pin "4093821cc9759ca5a3c6e527d4cc915fc3a5ad74")
+(package! pandoc-mode :pin "0e1a50717599e813e2b872f2af9f40b2272793fd")
+(package! centered-cursor-mode :pin "ebaeb80fba0bafdf6f95706308123dec2cf4b99f")
 (package! ssh-config-mode :pin "d560a0876a93ad4130baf33dae1b9405ad37a405")
 (package! smart-newline :pin "0553a9e4be7188352de1a28f2eddfd28e7436f94")
 (package! hardhat :pin "908cb130be3d56921a3687a00b974ba5eef3a11f")
-(package! fix-word :pin "e967dd4ac98d777deeede8b497d6337634c06df4")
-(package! string-inflection :pin "fd7926ac17293e9124b31f706a4e8f38f6a9b855")
-(package! ialign :pin "eca40b8b59ea713dba21b18f5b047a6c086b91dc")
-(package! s :pin "08661efb075d1c6b4fa812184c1e5e90c08795a9")
-(package! graphql-mode :pin "9740e4027bd9313697d5cac5caaa5b15626ab1da")
-(package! vundo :pin "0b8538f4e4c2f25cb9d5b75cf064ba8dceca6145")
+(package! fix-word :pin "02f9856e3fc8211cad8f6477431361f38ff04ab5")
+(package! string-inflection :pin "50ad54970b3cc79b6b83979bde9889ad9a9e1a9c")
+(package! ialign :pin "bc4d30d79f2f4b413288195ef19894ac0fd258b7")
+(package! s :pin "e957dcb0677da18b2bb60ad867db5df5c35b5616")
+(package! graphql-mode :pin "92136cf9b5a4dcd8c202c8dba9064b497776d2f7")
+(package! vundo :pin "d78b02ab89955f3a3273884f6d799889a2ef6b6f")
 
 (when (modulep! :lang web)
   (package! yarn-mode :pin "8239d4dc7d8a52fa1e3fa81bd32c904a359fcfc1"))
