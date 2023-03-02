2ac8e50ca1450273e00b40bcd60444a0dd4d2a76
Björn Larsson <b.e.larsson@gmail.com>
2023-03-02 20:24:23 +0100

    Fix bindings

--- +bindings.el
+++ +bindings.el
@@ -20,13 +20,6 @@
       (:prefix "n"
        "o" nil)
 
-      (:when (modulep! :lang org)
-       (:prefix "n"
-        :desc "Org Roam Capture Today" "c" #'org-roam-dailies-capture-today
-        :desc "Org Roam Capture" "C" #'org-roam-capture);; Override Doom binding
-       (:prefix "o"
-        :desc "Org Agenda" "A" (cmd! (org-agenda nil "a"))))
-
       (:when (modulep! :tools lsp)
        (:prefix "c"
         :desc "IMenu" "I" #'lsp-ui-imenu))
