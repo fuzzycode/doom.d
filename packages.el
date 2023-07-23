9bedaaeb35e95fb423d4972fc9bd5eb788cd683f
Björn Larsson <b.e.larsson@gmail.com>
2023-07-23 10:07:26 +0200

    Bump packages

--- packages.el
+++ packages.el
@@ -2,7 +2,7 @@
 
 
 ;; C++
-(package! ninja-mode :recipe (:files ("*.el")) :pin "adf9bddd73869084a505fac83246e55c35880079")
+(package! ninja-mode :recipe (:files ("*.el")) :pin "36843d387cb0621c1a288179af223d4f1410be73")
 (package! ff-c-style :recipe (:host github :repo "fuzzycode/ff-c-style"))
 
 (package! shader-mode :pin "fe5a1982ba69e4a98b834141a46a1908f132df15") ;; This is also a part of the csharp module but I only want this package from it
@@ -14,7 +14,7 @@
   (when IS-WINDOWS
     (package! ssh-agency :pin "a5377e4317365a3d5442e06d5c255d4a7c7618db"))
   (package! diff-dired :recipe (:host github :repo "fuzzycode/diff-dired"))
-  (package! magit-imerge :pin "5a1833d33e1516c7819521a5b35135527d9409f9")
+  (package! magit-imerge :pin "b7cfe49a197c2cf5948109921e053711b156389d")
   (package! magit-lfs :pin "cd9f46e1840270be27e2c2d9dcf036ff0781f66d")
   (package! rigid-tabs :pin "872a10c8751574c9610cba1800f541a6eda24997")
   (package! gitignore-templates :pin "d28cd1cec00242b688861648d36d086818b06099"))
@@ -27,15 +27,15 @@
   (package! demo-it :pin "e399fd7ceb73caeae7cb50b247359bafcaee2a3f")
   (package! org-super-agenda :pin "f4f528985397c833c870967884b013cf91a1da4a")
   (package! idle-org-agenda :pin "8e6052fc4923c30132052d67d794b76c92851c20")
-  (package! vulpea :pin "f4d3448b6ccdb314c5fe3defea66e750e1371a10")
+  (package! vulpea :pin "de199a16e294056e2368a2e031b19008cf9f9e52")
   (package! org-make-toc :pin "26fbd6a7e1e7f8e473fe3a5f74faec715c3a05aa")
-  (package! org-ql :pin "1d98c7d07c6f2af5c84e8358cb6c5db71e8f1006")
+  (package! org-ql :pin "eb5377320fcfd38354d6e9e3e655969ae3c0e052")
   (package! ox-gfm :pin "46faa67dbb3fb0cd7a76c3fe518f16e4195c22c7")
-  (package! ox-asciidoc :pin "a55ac6adef39124c9434be47fe9cc0c75c4bfea2"))
+  (package! ox-asciidoc :pin "3a8aad85c6df84155266ba5232f1cbadda8abc58"))
 
 ;;ELISP
 (when (modulep! :lang emacs-lisp)
-  (package! inspector :pin "8c952bade833d1b27ab4455f73a8f3393ce32322")
+  (package! inspector :pin "5f32ed1f9f7e6b66b1c4027cdf71c113dd650226")
   (package! eval-sexp-fu :pin "36d2fe3bcf602e15ca10a7f487da103515ef391a"))
 
 ;; MAIL
@@ -47,13 +47,12 @@
   (package! mu4e-views :pin "b02e360d36c54cd9a5f59cdf266be88214a966d2"))
 
 (when (modulep! :emacs dired)
-  (package! dired+ :pin "90fc0d5a110063278e3fe320dd903ec07be5dba7"))
+  (package! dired+ :pin "850f80756b4e95035735e4af3ce5da31f612de13"))
 
 (package! swedish-holidays :recipe (:host github :repo "fuzzycode/swedish-holidays"))
 (package! lang-mode :recipe (:host github :repo "fuzzycode/lang-mode"))
 (package! visual-regexp-steroids :pin "a6420b25ec0fbba43bf57875827092e1196d8a9e")
 (package! visual-regexp :pin "48457d42a5e0fe10fa3a9c15854f1f127ade09b5")
-(package! expand-region :pin "b70feaa644310dc2d599dc277cd20a1f2b6446ac")
 (package! sort-words :pin "7b6e108f80237363faf7ec28b2c58dec270b8601")
 (package! smart-backspace :pin "a10ec44ff325ec8c4c98b1a6e44e89e60a9aa4ac")
 (package! open-junk-file :pin "558bec7372b0fed4c4cb6074ab906535fae615bd")
@@ -64,8 +63,7 @@
 (package! hardhat :pin "908cb130be3d56921a3687a00b974ba5eef3a11f")
 (package! string-inflection :pin "50ad54970b3cc79b6b83979bde9889ad9a9e1a9c")
 (package! ialign :pin "bc4d30d79f2f4b413288195ef19894ac0fd258b7")
-(package! s :pin "dda84d38fffdaf0c9b12837b504b402af910d01d")
-(package! deadgrep :pin "f65a20d5c3d42aa649314f13976bc18020a9fc2a")
+(package! deadgrep :pin "b36dc1b2e5d5e8cd53c1977eeea9b6469ad42381")
 
 (package! chatgpt-shell) ;; Leave unpinned, it moves fast right now
 (package! dall-e-shell)
