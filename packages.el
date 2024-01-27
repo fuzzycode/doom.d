82e3cb6a2133620b18d3951aa4b8c59de05b8fcb
Björn Larsson <b.e.larsson@gmail.com>
2024-01-27 08:17:17 +0100

    Bump packages

--- packages.el
+++ packages.el
@@ -5,7 +5,7 @@
 (package! ccls :disable t)
 
 ;; C++
-(package! ninja-mode :recipe (:files ("*.el")) :pin "08cfea52fcd535d32bbca6abf893164c8bb2f7e9")
+(package! ninja-mode :recipe (:files ("*.el")) :pin "fd7067652cae480190bf13b2ee5475efdf09ac7d")
 (package! ff-c-style :recipe (:host github :repo "fuzzycode/ff-c-style"))
 
 (package! shader-mode :pin "fe5a1982ba69e4a98b834141a46a1908f132df15") ;; This is also a part of the csharp module but I only want this package from it
@@ -17,7 +17,7 @@
   (when IS-WINDOWS
     (package! ssh-agency :pin "a5377e4317365a3d5442e06d5c255d4a7c7618db"))
   (package! diff-dired :recipe (:host github :repo "fuzzycode/diff-dired"))
-  (package! magit-imerge :pin "b7cfe49a197c2cf5948109921e053711b156389d")
+  (package! magit-imerge :pin "34a057b452de7f856fd3bdef4a9e34309d2be9dc")
   (package! magit-lfs :pin "cd9f46e1840270be27e2c2d9dcf036ff0781f66d")
   (package! rigid-tabs :pin "9553118e76fcbc1d8f0bcb960de13c7e3f07b9df")
   (package! gitignore-templates :pin "d28cd1cec00242b688861648d36d086818b06099"))
@@ -25,28 +25,28 @@
 ;;ORG
 (when (modulep! :lang org)
   (package! demo-it :pin "e399fd7ceb73caeae7cb50b247359bafcaee2a3f")
-  (package! org-super-agenda :pin "4efc0bdfa3c9760256a651dc4d510658f41afe81")
-  (package! vulpea :pin "1b8445a0838c34c8e79cdd9ee3694bae55e421aa")
-  (package! org-make-toc :pin "469d6ea09f915de955ee322e0eb68b3f3085fef6")
-  (package! org-ql :pin "95abce2340efabecc6f07626eb3f28159929709f")
-  (package! ox-gfm :pin "46faa67dbb3fb0cd7a76c3fe518f16e4195c22c7")
-  (package! ox-asciidoc :pin "3a8aad85c6df84155266ba5232f1cbadda8abc58"))
+  (package! org-super-agenda :pin "ee3379ae92b90c084717fb2a7614060ce12283cb")
+  (package! vulpea :pin "e1ea8480daf3e480effdd7ba3799126295a4a59a")
+  (package! org-make-toc :pin "238be02dcf041a7635734769a041e848f0aeb10d")
+  (package! org-ql :pin "8d3c93b8838bd5d5347dd8962b68cc3cabcf0147")
+  (package! ox-gfm :pin "4f774f13d34b3db9ea4ddb0b1edc070b1526ccbb")
+  (package! ox-asciidoc :pin "c0b8db599ba1032820b5f9092e8f2d1e60b6c197"))
 
 ;;ELISP
 (when (modulep! :lang emacs-lisp)
-  (package! inspector :pin "c825b7f41bf10666563101d264ea5466f88726b3")
+  (package! inspector :pin "7fd65dddac9875c2e30e364b8702c0d72915c3ac")
   (package! eval-sexp-fu :pin "36d2fe3bcf602e15ca10a7f487da103515ef391a"))
 
 ;; MAIL
 (when (modulep! :email mu4e)
-  (package! mu4e-maildirs-extension :pin "cdc2e141d8ecd59508a5cd50d6d02120073bf4f1"))
+  (package! mu4e-maildirs-extension :pin "df5a7540d5eb55380417fb29694e97b064a4078f"))
 
 (when (and (modulep! :email mu4e)
            (featurep 'xwidget-internal))
   (package! mu4e-views :pin "b02e360d36c54cd9a5f59cdf266be88214a966d2"))
 
 (when (modulep! :emacs dired)
-  (package! dired+ :pin "36d7958a7226fe26a62cf26cba5e37dd14ddab70"))
+  (package! dired+ :pin "377ec04347b8dd8a91df1a33d820fa862fe0efa8"))
 
 (package! swedish-holidays :recipe (:host github :repo "fuzzycode/swedish-holidays"))
 (package! lang-mode :recipe (:host github :repo "fuzzycode/lang-mode"))
@@ -55,14 +55,14 @@
 (package! sort-words :pin "7b6e108f80237363faf7ec28b2c58dec270b8601")
 (package! smart-backspace :pin "a10ec44ff325ec8c4c98b1a6e44e89e60a9aa4ac")
 (package! open-junk-file :pin "558bec7372b0fed4c4cb6074ab906535fae615bd")
-(package! pandoc-mode :pin "da3f0f5238a8b1e5f09f2ec97e683dc488d25be0")
+(package! pandoc-mode :pin "340da8787d6305640d042af46d2bd609bd5a7518")
 (package! centered-cursor-mode :pin "67ef719e685407dbc455c7430765e4e685fd95a9")
 (package! ssh-config-mode :pin "d560a0876a93ad4130baf33dae1b9405ad37a405")
 (package! smart-newline :pin "0553a9e4be7188352de1a28f2eddfd28e7436f94")
 (package! hardhat :pin "908cb130be3d56921a3687a00b974ba5eef3a11f")
 (package! string-inflection :pin "50ad54970b3cc79b6b83979bde9889ad9a9e1a9c")
 (package! ialign :pin "bc4d30d79f2f4b413288195ef19894ac0fd258b7")
-(package! deadgrep :pin "415b69394960677a8a013d32a3cf67cb9956eef6")
+(package! deadgrep :pin "38abe362997d2f18633a75d04c09da751bf8085e")
 
 (package! chatgpt-shell) ;; Leave unpinned, it moves fast right now
 (package! dall-e-shell)
