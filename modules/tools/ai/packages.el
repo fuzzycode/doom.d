6641d6fafe5cc913af708d256496747894e3d412
Björn Larsson <b.e.larsson@gmail.com>
2025-05-02 09:35:07 +0200

    Bump Ai packages

--- modules/tools/ai/packages.el
+++ modules/tools/ai/packages.el
@@ -1,18 +1,18 @@
 ;;; tools/ai/packages.el -*- lexical-binding: t; -*-
 
 ;; gptel
-(package! gptel :pin "9349d90ffbb9cc067ae1553485f7391199c5ca74")
+(package! gptel :pin "db4e5c7ea39f7d58d8ca4856c98a6df71b1e18bf")
 (package! gptel-quick :recipe (:host github :repo "karthink/gptel-quick"))
 
 ;; copilot
-(package! copilot :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")) :pin "3e36a9825616e756f16c7c6a33f4edd5fb466260")
+(package! copilot :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")) :pin "6c8ad4e4b65f5eec026325db2ac5a0aaedd8b22c")
 
 ;; chatgpt-shell
-(package! chatgpt-shell :pin "5fc6c333b3bc91fba41871f5a89217804a66eabd")
+(package! chatgpt-shell :pin "51e0523d6c7abeb5b99ac93b0af033d28acc36b2")
 (package! dall-e-shell :pin "efec43ab3338e59f12165110e6c6c957345f6257")
 
-(package! mcp :recipe (:host github :repo "lizqwerscott/mcp.el"))
+(package! mcp :recipe (:host github :repo "lizqwerscott/mcp.el") :pin "d0f3b5e53c1eded4619e537881e69c4f6d5a2a94")
 
 (when (modulep! :lang org)
-  (package! ob-chatgpt-shell)
-  (package! ob-dall-e-shell))
+  (package! ob-chatgpt-shell :pin "754ddf54a99bd98427c91a6c7374757026f8bd45")
+  (package! ob-dall-e-shell :pin "9c052b7d55b33d7e76747a4dde0eb130a17c33e1"))
